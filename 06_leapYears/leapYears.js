19095c368926345e968452aa76f2a7d2f61d5b81
David <czdavid93@gmail.com>
2023-05-19 23:46:31 +0100

    excercise 1-5

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  ///year divisible by 4 YES
+  ///divisible by 100 NO.....unless they are divisible by 400
 };
 
 // Do not edit below this line
